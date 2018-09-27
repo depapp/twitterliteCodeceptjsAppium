dee7ada6b7f135d53756496ef0342e20874ff491
depapp <depa.article@gmail.com>
2018-09-27 16:48:59 +0700

    some changes

--- pages/auth.js
+++ pages/auth.js
@@ -1,34 +1,35 @@
-var Chance = require('chance')
+var Chance = require("chance")
 var chance = new Chance()
 
 const I = actor()
 
 module.exports = {
   field: {
-    landingpageField: '//android.view.View[@index=1]',
-    usernameField: '//android.widget.EditText[@index=0]',
-    passwordField: '//android.widget.EditText[@index=1]',
+    landingpageField: "//android.view.View[@index=1]",
+    usernameField: "//android.widget.EditText[@index=0]",
+    passwordField: "//android.widget.EditText[@index=1]",
     nameField: "//android.widget.EditText[@text='Name']",
     emailField: "//android.widget.EditText[@text='Email']",
-    homeField: '//android.view.View[@index=1]',
-    loginFailedField: '//android.view.View[@index=2]'
+    passwordRegField: "//android.widget.EditText[@text='••••••••']",
+    homeField: "//android.view.View[@index=1]",
+    loginFailedField: "//android.view.View[@index=2]"
   },
 
   button: {
-    loginButton: '//android.view.View[@index=4]',
-    signupButton: '//android.view.View[@index=3]',
-    emailInsteadButton: '//android.widget.Button[@index=6]',
-    submitLoginButton: '//android.widget.Button[@index=2]',
-    nextSignupButton: '//android.widget.Button[@index=1]',
-    submitSignupButton: '//android.widget.Button[@index=6]'
+    loginButton: "//android.view.View[@index=4]",
+    signupButton: "//android.view.View[@index=3]",
+    emailInsteadButton: "//android.widget.Button[@index=6]",
+    submitLoginButton: "//android.widget.Button[@index=2]",
+    nextSignupButton: "//android.widget.Button[@index=1]",
+    submitSignupButton: "//android.widget.Button[@index=1]"
   },
 
   value: {
-    usernameValue: chance.first(),
+    usernameValue: "educlopedia",
     usernameValueInvalid: chance.first(),
     nameValue: chance.first(),
     emailValue: chance.email({
-      domain: 'mailinator.com'
+      domain: "mailinator.com"
     }),
     passwordValue: chance.string(),
     passwordValueInvalid: chance.last()
--- signup_test.js
+++ signup_test.js
@@ -1,13 +1,13 @@
 Feature('Signup')
 
-Scenario.only('Signup with Valid Credentials', async(I, authPage) => {
-	I.seeElement(authPage.field.landingpageField)
-	I.click(authPage.button.signupButton)
-	I.click(authPage.button.emailInsteadButton)
-	I.fillField(authPage.field.nameField, authPage.value.nameValue)
-	I.fillField(authPage.field.emailField, authPage.value.emailValue)
-	I.fillField(authPage.field.passwordRegField, authPage.value.passwordValue)
-	I.click(authPage.button.nextSignupButton)
-	I.click(authPage.button.submitSignupButton)
-	I.wait(5)
+Scenario.only('Signup with Valid Credentials', async (I, authPage) => {
+    I.seeElement(authPage.field.landingpageField)
+    I.click(authPage.button.signupButton)
+    I.click(authPage.button.emailInsteadButton)
+    I.fillField(authPage.field.nameField, authPage.value.nameValue)
+    I.fillField(authPage.field.emailField, authPage.value.emailValue)
+    I.fillField(authPage.field.passwordRegField, authPage.value.passwordValue)
+    I.click(authPage.button.nextSignupButton)
+    I.click(authPage.button.submitSignupButton)
+    I.wait(5)
 });
